cea313675fd5918c15731d48bfa065a41fdc75e5
GiaPhong132 <76869112+GiaPhong132@users.noreply.github.com>
2022-12-29 22:50:28 +0700

    first commit

---
+++ views/main/footer.php
@@ -0,0 +1,105 @@
+<footer id="footer">
+
+    <div class="footer-top">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 footer-contact">
+                    <h3>Trụ sở chính</h3>
+                    <p>
+                        Số 8 đường số 4 <br>
+                        Phường 2, Quận 1<br>
+                        TP. Hồ Chí Minh, Việt Nam <br><br>
+                        <strong>Điện thoại:</strong> 012 345 6789<br>
+                        <strong>Email:</strong> IDEdevelopment@BKSHOPE.com.vn<br>
+                    </p>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>JOIN US</h4>
+                    <ul>
+                        <li><i class="fa-solid fa-chevrons-right"></i> <a href="">Facebook</a></li>
+                        <li><i class="fa-solid fa-chevrons-right"></i> <a href="">Youtube</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-6 footer-links">
+                    <h4>CONTACT US</h4>
+                    <ul>
+                        <li><i class="fa-solid fa-chevrons-right"></i> <a href="index.php?page=main&controller=register&action=index">123456789</a></li>
+                        <li><i class="fa-solid fa-chevrons-right"></i> <a href="">987654321</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-4 col-md-6 footer-newsletter">
+                    <h4>Đăng ký nhận tin tức mới nhất</h4>
+                    <p>Những thông tin và tin tức mới nhất về BKSHOPE sẽ được gửi đến bạn qua email bạn điền dưới đây</p>
+                    <form action="" method="post">
+                        <input type="email" name="email"><input type="submit" value="Đăng ký">
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+        <div class="me-md-auto text-center text-md-start">
+            <div class="copyright">
+                &copy; Copyright <strong><span>BKSHOPE</span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                Designed by <a href="index.php?page=admin&controller=layouts&action=index">BKSHOPE</a>
+            </div>
+            <!-- Back to top button -->
+            <!-- <a href="#" class="btn back-to-top"></a> -->
+            <script>
+                window.onscroll = function() {
+                    scrollFunction()
+                };
+
+                function scrollFunction() {
+                    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                        document.getElementById("src-top").style.display = "block";
+                    } else {
+                        document.getElementById("src-top").style.display = "none";
+                    }
+                }
+
+                // When the user clicks on the button, scroll to the top of the document
+                function topFunction() {
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, '400');
+                }
+            </script>
+
+</footer><!-- End Footer -->
+<button onclick="topFunction()" id="src-top" title="Go to top"><i class="fa fa-chevron-up"></i></button>
+
+
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+    AOS.init();
+</script>
+
+
+<script type="text/javascript" src="/Source_code/public/js/main.js"></script>
+<script type="text/javascript" src="/Source_code/public/js/custom.js"></script>
+
+<!-- JAVASCRIPTS -->
+<script src="/Source_code/public/plugins/jquery/jquery.js"></script>
+<script src="/Source_code/public/plugins/popper/popper.min.js"></script>
+<script src="/Source_code/public/plugins/bootstrap/bootstrap.min.js"></script>
+<script src="/Source_code/public/plugins/owl-carousel/owl.carousel.min.js"></script>
+
+<script src="/Source_code/public/plugins/syotimer/jquery.syotimer.min.js"></script>
+<script src="/Source_code/public/plugins/aos/aos.js"></script>
+
+<script src="/Source_code/public/js/custom.js"></script>
+
+
+</body>
+
+</html>
---
+++ views/main/layouts/index.php
@@ -0,0 +1,10 @@
+<?php require_once("views/main/navbar.php"); ?>
+
+<?php require_once("home/hero.php"); ?>
+<?php require_once("home/about.php"); ?>
+<?php require_once("home/wedo.php"); ?>
+<?php require_once("home/clients.php"); ?>
+<?php require_once("home/services.php"); ?>
+<?php require_once("home/tesinomianl.php"); ?>
+<?php require_once("home/end.php"); ?>
+<?php require_once("views/main/footer.php"); ?>
